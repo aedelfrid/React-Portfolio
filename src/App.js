13f84bb8433dc068936650943e90c0fa48a1f87b
aedelfrid <almck51@gmail.com>
2023-10-11 00:48:46 -0300

    added a simple carousel

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/Header';
+import Carousel from './components/Carousel';
+import './assets/css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+        <Carousel />
+    </div>
+  );
+}
+
+export default App;
